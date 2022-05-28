331a60d894ebe860e4c4f16ec81467b57e32dc89
CasaBright <casabright@pm.me>
2022-05-28 10:26:58 -0500

    Set credits text to enable end screen.

---
+++ Assets/Sprites/Credits.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    private Animator anim;  
+    [SerializeField] GameObject endScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowEndScreen()
+    {
+        endScreen.SetActive(true);
+    }
+}
